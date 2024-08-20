2913dbb5ac4737072fbc6d263c0926184450f2b8
Augustika Pal <95644063+AugustikaPal@users.noreply.github.com>
2024-08-20 11:30:10 +0530

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,5 @@
+from crewai_tools import YoutubeChannelSearchTool
+
+# Initialize the tool with a specific Youtube channel handle to target your search
+yt_tool = YoutubeChannelSearchTool(Topic='Simple Linear Regression Algorithm Indepth Maths Intuition With Notes In Hindi')
+
